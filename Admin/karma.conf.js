0027dc1f781882b3ffad0266a217561b560becee
NguyenDuongQuang <ndq1234pn@gmail.com>
2023-11-16 15:38:17 +0700

    admin
    admin

--- Admin/karma.conf.js
+++ Admin/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/my-adminlte3-demo-app'),
+      dir: require('path').join(__dirname, './coverage/font-end-admin'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
